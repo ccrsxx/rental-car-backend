72be7244a598e7601446e47b7cf37cb4cbecd3d6
ccrsxx <aminrisal@gmail.com>
2023-11-19 12:56:15 +0700

    feat: print out env preview link by heroku

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -6,6 +6,11 @@ import * as Types from '../../libs/types/common.js';
  * @returns {void}
  */
 export function ping(_req, res) {
+  const { HEROKU_APP_NAME, HEROKU_BRANCH, HEROKU_PR_NUMBER } = process.env;
+
+  // eslint-disable-next-line no-console
+  console.log({ HEROKU_APP_NAME, HEROKU_BRANCH, HEROKU_PR_NUMBER });
+
   res.status(200).json({
     message: 'Ping successfully on dev',
     documentation: `${PUBLIC_URL}/docs`
